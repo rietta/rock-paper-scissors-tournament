d1ac6cc94b9709fea743424580d1dd6e88939b73
Frank Rietta <frank@rietta.com>
2024-05-21 22:46:51 -0400

    Get the choices

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,11 +4,19 @@ function computer_random_choice() {
   return  selection_options[(Math.random() * selection_options.length) | 0]
 }
 
+function get_player_choice() {
+  return document.querySelector("input[type='radio'][name=item]:checked").value;
+}
 function play() {
-  player_choice_form = document.getElementById('player_choice_form');
-  player_choice = player_choice_form.elements['item'].value;
-  computer_choice = computer_random_choice();
+  // For perceived fairness, always pick the computer answer before reading
+  // the user's choice
+  computer_choice_val = computer_random_choice();
+  player_choice_val = get_player_choice();
   
-  console.log(item);
+  computer_choice = document.getElementById('computer_choice');
+  computer_choice.textContent = computer_choice_val;
+
+  player_choice = document.getElementById('player_choice');
+  player_choice.textContent = player_choice_val; 
 }
 
