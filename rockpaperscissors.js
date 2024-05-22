8338df8824d8803a7b5b668468bde10cfb7e8818
Frank Rietta <frank@rietta.com>
2024-05-22 13:41:29 -0400

    Draft of method to detect winner.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,4 +1,7 @@
 const selection_options = ['rock', 'paper', 'scissors'];
+const tie = 0;
+const p1 = 1;
+const p2 = 2;
 
 function computer_random_choice() {
   return  selection_options[(Math.random() * selection_options.length) | 0]
@@ -6,6 +9,33 @@ function computer_random_choice() {
 
 function get_player_choice() {
   return document.querySelector("input[type='radio'][name=item]:checked").value;
+}
+
+function winner(p1_choice, p2_choice) {
+  if(p1_choice === p2_choice) {
+    return tie;
+  } else if (p1_choice === 'rock') {
+    if (p2_choice === 'scissors') {
+      return p1;
+    } else if (p2_choice === 'paper') {
+      return p2;
+    }
+  } else if (p1_choice === 'paper') {
+    if (p2_choice === 'rock') {
+      return p1;
+    } else if (p2_choice === 'scissors') {
+      return p2;
+    }
+  } else if (p1_choice === 'scissors') {
+    if (p2_choice === 'paper') {
+      return p1;
+    } else if (p2_choice === 'rock') {
+      return p2;
+    }
+  } else {
+    console.log("Odd situation that should not happen. Returning tie.");
+    return tie;
+  }
 }
 function play() {
   // For perceived fairness, always pick the computer answer before reading
