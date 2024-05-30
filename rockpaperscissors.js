aa368b87c5c13d45a9d1a8c194e2379dc0457c2a
Frank Rietta <frank@rietta.com>
2024-05-30 08:35:08 -0400

    Update the match display on each turn

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -40,34 +40,67 @@ function winner(p1_choice, p2_choice) {
   }
 }
 
-function show_player_one_winner() {
-
-}
-
-function show_computer_winner() {
-
-}
-
-function show_tie() {
-
-}
-
 function update_text_content(element_id, value) {
   element = document.getElementById(element_id);
   element.textContent = value;
 }
 
 function update_choice_display(player_number, choice) {
+
+  if (choice === "") {
+    choice_icon = "";
+  } else { 
+    choice_icon = selection_icons[choice];
+  }
+
   icon_selector = "p" + player_number + "_choice_icon";
-  update_text_content(icon_selector, selection_icons[choice]);
+  update_text_content(icon_selector, choice_icon);
 
   label_selector = "p" + player_number + "_choice_label";
   update_text_content(label_selector, choice);
 }
 
-function reset_game() {
-
+function display_element(player_number) {
+  element_id = "p" + player_number + "_display";
+  element = document.getElementById(element_id);
+  return element;
 }
+
+function remove_winner_border(player_number) {
+  element = display_element(player_number);
+  element.classList.remove('winner');
+  element.classList.remove('tie');
+}
+
+function add_winner_border(player_number) {
+  display_element(player_number).classList.add('winner');
+}
+
+function add_tie_border(player_number) {
+  display_element(player_number).classList.add('tie');
+}
+
+function reset_display() {
+  remove_winner_border(p1);
+  update_choice_display(p1, "");
+  remove_winner_border(p2);
+  update_choice_display(p2, "");
+}
+
+function reset_game() {
+  reset_display();
+}
+
+
+function call_winner(player_number) {
+  add_winner_border(player_number);
+}
+
+function call_tie() {
+  add_tie_border(p1);
+  add_tie_border(p2);
+}
+
 
 function play() {
   // For perceived fairness, always pick the computer answer before reading
@@ -75,16 +108,17 @@ function play() {
   computer_choice_val = computer_random_choice();
   player_choice_val = player_selected_choice();
   
+  reset_display();
   update_choice_display(2, computer_choice_val);
   update_choice_display(1, player_choice_val);
 
   result = winner(player_choice_val, computer_choice_val);
-  // if (result === p1) {
-  //   show_player_one_winner();
-  // } else if (result === p2) {
-  //   show_computer_winner();
-  // } else {
-  //   show_tie();
-  // }
+  if (result === p1) {
+    call_winner(p1);
+  } else if (result === p2) {
+    call_winner(p2);
+  } else {
+    call_tie();
+  }
 }
 
